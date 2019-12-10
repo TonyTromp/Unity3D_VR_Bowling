2734d56810859d343807d12d8ef2b7d29c6b4b28
Tony Tromp <tony@eureka.ai>
2019-12-10 11:32:00 +0100

    Initial load...

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speedFactor = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();       
+    }
+
+    void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Debug.LogFormat("x-Axis {0} y-Axis: {1}",moveHorizontal,moveVertical);
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speedFactor);
+
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("SPACE");
+            rb.AddForce(0.0f,300.0f, 0.0f);
+        }
+    }
+
+}
+
